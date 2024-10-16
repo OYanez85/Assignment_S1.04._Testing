2780c1e6912a11aa8171da22c50249a5765bc36e
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-10-16 12:44:42 +0200

    Update LibraryTest.java

--- Level_1-JUnit/Exercise_1/src/test/java/com/example/LibraryTest.java
+++ Level_1-JUnit/Exercise_1/src/test/java/com/example/LibraryTest.java
@@ -1,37 +1,59 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
+    private Library library;
+
+    @BeforeEach
+    public void setup() {
+        library = new Library();
+        library.addBook(new Book("The Hobbit"));
+        library.addBook(new Book("1984"));
+        library.addBook(new Book("Harry Potter"));
+    }
 
     @Test
     public void testLibraryIsNotNull() {
-        Library library = new Library();
         assertNotNull(library.getBooks());
     }
 
     @Test
     public void testAddBooks() {
-        Library library = new Library();
-        library.addBook(new Book("Book 1"));
-        library.addBook(new Book("Book 2"));
-        assertEquals(2, library.getBooks().size());
+        assertEquals(3, library.getBooks().size());
     }
 
     @Test
     public void testGetBookAtPosition() {
-        Library library = new Library();
-        Book book = new Book("Book 1");
-        library.addBook(book);
-        assertEquals(book, library.getBookAtPosition(0));
+        Book book = library.getBookAtPosition(0);
+        assertEquals(new Book("1984"), book);  // Sorted alphabetically
     }
 
     @Test
     public void testRemoveBookByTitle() {
-        Library library = new Library();
-        library.addBook(new Book("Book 1"));
-        library.removeBookByTitle("Book 1");
-        assertEquals(0, library.getBooks().size());
+        library.removeBookByTitle("1984");
+        assertEquals(2, library.getBooks().size());
+    }
+
+    @Test
+    public void testNoDuplicateBooks() {
+        library.addBook(new Book("1984"));
+        assertEquals(3, library.getBooks().size());  // Duplicate should not be added
+    }
+
+    @Test
+    public void testListIsSorted() {
+        library.addBook(new Book("A Tale of Two Cities"));
+        assertEquals("A Tale of Two Cities", library.getBookAtPosition(0).getTitle());
+    }
+
+    @Test
+    public void testOutOfBoundsIndex() {
+        Exception exception = assertThrows(IndexOutOfBoundsException.class, () -> {
+            library.getBookAtPosition(5);
+        });
+        assertEquals("Invalid index: 5", exception.getMessage());
     }
 }
