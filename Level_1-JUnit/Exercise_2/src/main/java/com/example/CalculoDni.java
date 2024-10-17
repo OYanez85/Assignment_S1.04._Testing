20808fac70a37dc89324509cc56e3ec33fe2cf10
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-10-17 09:38:44 +0200

    Update CalculoDni.java

--- Level_1-JUnit/Exercise_2/src/main/java/com/example/CalculoDni.java
+++ Level_1-JUnit/Exercise_2/src/main/java/com/example/CalculoDni.java
@@ -1,8 +1,26 @@
 package com.example;
 
-public class CalculoDni {
-    public static char calculateDniLetter(int dniNumber) {
-        String dniLetters = "TRWAGMYFPDXBNJZSQVHLCKE"; // Correct DNI letters
-        return dniLetters.charAt(dniNumber % 23);
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalculoDniTest {
+    
+    @ParameterizedTest
+    @CsvSource({
+        "12345678, Z", // Example DNI numbers and expected letters
+        "23456789, A",
+        "34567890, B",
+        "45678901, C",
+        "56789012, D",
+        "67890123, E",
+        "78901234, F",
+        "89012345, G",
+        "90123456, H",
+        "10234567, I"
+    })
+    public void testCalculateDniLetter(int dniNumber, char expectedLetter) {
+        assertEquals(expectedLetter, CalculoDni.calculateDniLetter(dniNumber));
     }
 }
